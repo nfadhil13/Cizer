f287af53932901e3b6d54265590a0f9c2053f29f
Mohamad Naufal Fadhil <naufalfadhil12344@gmail.com>
2021-09-23 11:57:12 +0700

    Change Mediaquery to Layoutbuilder

--- lib/responsive_layout.dart
+++ lib/responsive_layout.dart
@@ -20,14 +20,14 @@ class ResponsiveLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    MediaQuery.of(context).size;
-    if(Cizer.isMobile){
-      return mobileBody();
-    }else if(Cizer.isTablet){
-      return tabletBody?.call() ?? mobileBody();
-    }
-    return desktopBody?.call() ?? mobileBody();
-
+    return LayoutBuilder(builder: (_, __) {
+      if(Cizer.isMobile){
+        return mobileBody();
+      }else if(Cizer.isTablet){
+        return tabletBody?.call() ?? mobileBody();
+      }
+      return desktopBody?.call() ?? mobileBody();
+    });
   }
 }
 
